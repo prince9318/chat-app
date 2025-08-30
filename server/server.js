cefa2fef231a1caab35c9a5f71ed5860fc89a205
Prince Vishwakarma <pko62547@gmail.com>
2025-08-30 19:40:29 +0530

    add frontend-url

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 // Initialize socket.io server
 export const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://chat-app-ud16.vercel.app/login",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
