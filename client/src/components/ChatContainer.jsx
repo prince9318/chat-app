6a7e48d226513727b7c389214c2be5f5bd3b0d6b
Prince Vishwakarma <pko62547@gmail.com>
2026-08-05 14:23:21 +0530

    fix ui issue

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -852,32 +852,11 @@ const ChatContainer = () => {
             }`}
             aria-label="Send"
           >
-            <svg
-              viewBox="0 0 46 46"
-              className={`w-5 h-5 ${hasMessageText ? "" : "opacity-45"}`}
-              aria-hidden="true"
-            >
-              <defs>
-                <linearGradient
-                  id="sendIconGradient"
-                  x1="23"
-                  y1="0"
-                  x2="23"
-                  y2="46"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stopColor="#C263FE" />
-                  <stop offset="1" stopColor="#7D36FE" />
-                </linearGradient>
-              </defs>
-              <circle cx="23" cy="23" r="23" fill="url(#sendIconGradient)" />
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M18.3739 27.7131C19.222 29.2715 21.9243 34.2198 21.9243 34.2198C21.9243 34.2198 31.9224 14.8584 32.0811 14.541L32.0938 14.4839L18.3739 27.7131ZM11.7676 23.4282C11.7676 23.4282 16.4003 26.2093 17.6997 26.9812L31.4463 13.9062C29.8822 14.6642 11.7676 23.4282 11.7676 23.4282Z"
-                fill="white"
-              />
-            </svg>
+            <img
+              src={assets.send_button}
+              alt=""
+              className={`w-5 h-5 ${hasMessageText ? "invert" : "opacity-45"}`}
+            />
           </button>
         </div>
       </div>
